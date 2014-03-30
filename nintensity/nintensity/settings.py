6c47e3d0f4f64d7467f2b4ed557dd9c042306d9b
Gary Guangyu Pei <guangyu.pei@icloud.com>
2014-03-30 16:59:14 -0700

    Change to Postgresql from sqlite

--- nintensity/nintensity/settings.py
+++ nintensity/nintensity/settings.py
@@ -60,12 +60,23 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'nintensity.db'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'nintensity.db'),
-    }
-}
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nintensity',
+        'USER': 'fitgoalsuser',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+	        }
+	    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
