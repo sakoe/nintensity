820cfe3c4324c6bfee5456c6030ed0810d49606d
Gary Pei <pei@rf9.(none)>
2014-04-03 18:35:20 -0700

    Add first model

--- nintensity/fitgoals/admin.py
+++ nintensity/fitgoals/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from fitgoals.models import WorkoutLog
 
-# Register your models here.
+class WorkoutLogAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'workout_name',
+        'workout_units',
+        'workout_date',
+        'created_date')
+    readonly_fields = ['created_date']
+
+admin.site.register(WorkoutLog, WorkoutLogAdmin)
--- nintensity/fitgoals/models.py
+++ nintensity/fitgoals/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class WorkoutLog(models.Model):
+    workout_name = models.CharField(max_length=128)
+    workout_units = models.TextField(blank=True)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+    workout_date = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.workout_name
+    
+    def get_absolute_url(self):
+        from django.core.urlresolvers import reverse
+        return reverse('people.views.details', args=[str(self.id)])
