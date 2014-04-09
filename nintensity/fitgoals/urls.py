43dc8306fcf4a3ca96beca9f3336f738033cb48d
ns <N/A>
2014-04-09 15:52:43 -0700

    Began to layout fitgoals a little more on the front end. Began CSS rewrite.

--- nintensity/fitgoals/urls.py
+++ nintensity/fitgoals/urls.py
@@ -3,5 +3,11 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('fitgoals.views',
     url(r'^$',
         'root_view',
-        name="site_index"),
+        name='root_view'),
+    url(r'^teams/',
+        'teams_view',
+        name='teams_view'),
+    url(r'^events/',
+        'events_view',
+        name='events_view'),
 )
