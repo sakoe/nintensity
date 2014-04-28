d153d0b207a44fa000f789f86dc8e572f22b03a5
Gary Pei <pei@rf9.(none)>
2014-04-28 13:40:25 -0700

    Setup unit tests for fitgoals.

--- nintensity/fitgoals/tests.py
+++ nintensity/fitgoals/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+"""
+These are unit tests for using the fitgoals module.
+"""
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from fitgoals.models import WorkoutType
+
+
+class FitGoalsTestCase(TestCase):
+    fixtures = ['fitgoals_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+
+class WorkoutTypeTestCase(TestCase):
+
+    def test_unicode(self):
+        expected = "Cycling"
+        wt = WorkoutType(workout_type=expected)
+        actual = unicode(wt)
+        self.assertEqual(expected, actual)
