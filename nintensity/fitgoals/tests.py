08f91a725894f9e34d39cb7a1b5b2b558366ed34
Gary Pei <pei@rf9.(none)>
2014-04-28 14:53:39 -0700

    Add tests on workoutlog

--- nintensity/fitgoals/tests.py
+++ nintensity/fitgoals/tests.py
@@ -4,7 +4,7 @@ These are unit tests for using the fitgoals module.
 
 from django.test import TestCase
 from django.contrib.auth.models import User
-from fitgoals.models import WorkoutType
+from fitgoals.models import WorkoutType, WorkoutLog
 
 
 class FitGoalsTestCase(TestCase):
@@ -21,3 +21,14 @@ class WorkoutTypeTestCase(TestCase):
         wt = WorkoutType(workout_type=expected)
         actual = unicode(wt)
         self.assertEqual(expected, actual)
+
+class WorkoutLogTestCase(FitGoalsTestCase):
+
+    def setUp(self):
+        super(WorkoutLogTestCase,self).setUp()
+        self.user = User.objects.get(pk=2)
+        wl = WorkoutLog(workout_name='Cycling Excercise', user = self.user)
+        wl.save()
+
+    def test_workoutlog(self):
+        
