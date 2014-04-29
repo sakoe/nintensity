52efc138fa8ceeca6ad1739ce9291be2f104d4ae
Sako <spe@i-sako.com>
2014-04-28 23:17:26 -0700

    Change order of fields.

--- nintensity/userprofile/forms.py
+++ nintensity/userprofile/forms.py
@@ -6,4 +6,4 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ('fitbit_id','runkeeper_id')
+        fields = ('screen_name','gender','fitbit_id','runkeeper_id')
