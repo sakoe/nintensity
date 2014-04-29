6a271aed144b96769a1f0b66d5cf3ff68cf84c9f
Sako <spe@i-sako.com>
2014-04-28 23:20:26 -0700

    Add message to view.

--- nintensity/userprofile/views.py
+++ nintensity/userprofile/views.py
@@ -3,14 +3,20 @@ from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
 from forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 @login_required
 def user_profile(request):
-    # import pdb; pdb.set_trace()
+    """
+    Gets the user profile information if it exists, allows user to update it. 
+    Displays a flash message on successful updates. 
+
+    """
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=request.user.profile)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Settings updated.')
             return HttpResponseRedirect('')
     else:
         user = request.user
