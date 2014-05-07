e2c48e77c9ffdf020bbed5384c93fb764424e667
Gary Guangyu Pei <guangyu.pei@icloud.com>
2014-05-07 15:26:20 -0700

    Add permission group by default

--- nintensity/fitgoals/__init__.py
+++ nintensity/fitgoals/__init__.py
@@ -1,5 +1,19 @@
 """fitgoals app
 
-.. moduleauthor:: Gary Pei <gary_pei@yahoo.com>
+.. moduleauthor:: Gary Pei
 
 """
+
+from django.db.models.signals import post_syncdb
+from django.conf import settings
+from django.contrib.auth.models import Group, Permission
+import models
+from models import WorkoutLog
+
+def create_fitgoal_group(sender, **kwargs):
+    """
+    create default group
+    """
+    group, created = Group.objects.get_or_create(name=settings.DEFAULT_GROUP_NAME)
+
+post_syncdb.connect(create_fitgoal_group, sender=models)
