778b9030abf212d2252ea65f3651b9b9479aee9c
Gary Guangyu Pei <guangyu.pei@icloud.com>
2014-05-07 19:50:53 -0700

    Autopep8

--- nintensity/fitgoals/management/__init__.py
+++ nintensity/fitgoals/management/__init__.py
@@ -7,18 +7,20 @@ from django.conf import settings
 from django.contrib.auth.models import Group, Permission
 import fitgoals.models
 
+
 def create_fitgoal_group(sender, **kwargs):
     """
     create default group
     """
-    group, created = Group.objects.get_or_create(name=settings.DEFAULT_GROUP_NAME)
+    group, created = Group.objects.get_or_create(
+        name=settings.DEFAULT_GROUP_NAME)
     if created:
-	plist = ['add_workoutlog', 'change_workoutlog', 'delete_workoutlog',
-		 'add_teammember', 'change_teammember', 'delete_teammember',
-		 'add_event', 'change_event', 'delete_event',
-		 'add_team', 'change_team', 'delete_team']
-	for p in plist:
-	    perm = Permission.objects.get(codename=p)
+        plist = ['add_workoutlog', 'change_workoutlog', 'delete_workoutlog',
+                 'add_teammember', 'change_teammember', 'delete_teammember',
+                 'add_event', 'change_event', 'delete_event',
+                 'add_team', 'change_team', 'delete_team']
+        for p in plist:
+            perm = Permission.objects.get(codename=p)
             group.permissions.add(perm)
         group.save()
 
