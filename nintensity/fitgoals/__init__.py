59e9395f7bce5f69e997ab4d2bed26a1def3eac8
Gary Guangyu Pei <guangyu.pei@icloud.com>
2014-05-07 18:26:00 -0700

    Move post_syncdb to management and it worked.

--- nintensity/fitgoals/__init__.py
+++ nintensity/fitgoals/__init__.py
@@ -3,17 +3,3 @@
 .. moduleauthor:: Gary Pei
 
 """
-
-from django.db.models.signals import post_syncdb
-from django.conf import settings
-from django.contrib.auth.models import Group, Permission
-import models
-from models import WorkoutLog
-
-def create_fitgoal_group(sender, **kwargs):
-    """
-    create default group
-    """
-    group, created = Group.objects.get_or_create(name=settings.DEFAULT_GROUP_NAME)
-
-post_syncdb.connect(create_fitgoal_group, sender=models)
