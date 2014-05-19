259b2b15d05224c196c464ec7c9faff15bbac340
ns <N/A>
2014-05-19 12:40:38 -0700

    revert forgotten-about change to userprofile/views.py that was committed by accident.

--- nintensity/userprofile/views.py
+++ nintensity/userprofile/views.py
@@ -8,11 +8,11 @@ from django.contrib import messages
 @login_required
 def user_profile(request):
     """
-    Gets the user profile information if it exists and allows user to update it.
-    Creates a new profile for the logged in user if none exists. 
-    Displays a flash message on successful updates. 
+Gets the user profile information if it exists and allows user to update it.
+Creates a new profile for the logged in user if none exists.
+Displays a flash message on successful updates.
 
-    """
+"""
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=request.user.profile)
         if form.is_valid():
@@ -32,25 +32,3 @@ def user_profile(request):
         args['form'] = form
 
         return render(request, 'profile.html', args)
-
-    # if request.method == 'POST':
-    #     form = UserProfileForm(request.POST, instance=request.user.profile)
-    #     if form.is_valid():
-    #         form.save()
-    #         messages.success(request, 'Settings updated.')
-    #         return HttpResponseRedirect('')
-    #     else:
-    #         return HttpResponseRedirect('')
-    # else:
-    #     user = request.user
-    #     profile = user.profile
-    #     form = UserProfileForm(instance=profile)
-
-    #     args = {}
-    #     args.update(csrf(request))
-
-    #     args['form'] = form
-
-    #     return render(request, 'profile.html', args)
-
-
